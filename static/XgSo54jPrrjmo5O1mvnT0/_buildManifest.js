05a3bee1d654fa27680325f6b4fc18261437dcd7
Arjun Srivastava <arjunsrivastava01@gmail.com>
2023-05-02 23:25:15 -0700

    Updates

---
+++ static/XgSo54jPrrjmo5O1mvnT0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/361-0e454ddaa2778e0d.js","static/chunks/pages/index-bed0e930a480ea77.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
